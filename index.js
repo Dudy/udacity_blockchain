1e79a3b16752754a3e88c915b00e86d22669a0d6
Dirk Podolak <dirk.podolak@gmail.com>
2018-09-16 21:57:14 +0200

    project 4 // add REST endpoint to submit a registration, store registration and respond with appropriate data

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+"use strict";
+
 const express = require('express')
 const app = express()
 const Blockchain = require('./simpleChain.js').Blockchain;
 const Block = require('./simpleChain.js').Block;
+const levelSandbox = require('./levelSandbox.js');
 
 app.use(express.json());
 
@@ -28,5 +31,30 @@ async function postBlock(req, res) {
 
 app.get('/block/:blockheight', getBlock);
 app.post('/block', postBlock);
+
+// <project 4>
+
+const SHA256 = require('crypto-js/sha256');
+
+// 10 minutes = 600 seconds = 600000 millisecons
+const registration_time_duration = 10 * 60 * 1000;
+
+async function registrationRequest(req, res) {
+  let message = SHA256((Math.random() * (1 << 64)).toString()).toString();
+  let blockchainid = req.body.blockchainid;
+  let timestamp = new Date().getTime();
+
+  await levelSandbox.store("star_registration_request_" + blockchainid, timestamp);
+
+  res.send({
+    "message": message,
+    "timestamp": timestamp,
+    "timeRemainingInMs": registration_time_duration - new Date().getTime() + timestamp
+  });
+}
+
+app.post('/star/registration', registrationRequest);
+
+// </project 4>
 
 app.listen(8000, () => console.log('Example app listening on port 8000!'))
